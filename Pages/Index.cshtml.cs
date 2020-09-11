4cf3ee9e539bbe0ce7bc7a61cc0a586eaa8567d7
JamesH <jamesmichaelhickey@gmail.com>
2020-09-11 00:45:27 -0300

    new project with marten hooked up to postgres

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Marten;
+using Marten.Events;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace EventSourcedCallAudit.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IDocumentStore _store;
+
+        public IndexModel(IDocumentStore store)
+        {
+            this._store = store;
+        }
+
+        public void OnGet()
+        {
+            using var session = this._store.OpenSession();
+            // Add some querying here...
+        }
+    }
+}
